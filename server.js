145ed8a1aad18a251757e310d349dd0325aa7e06
himanshudube97 <himanshu.dube13@gmail.com>
2024-01-13 03:50:24 +0530

    changes added

--- server.js
+++ server.js
@@ -38,14 +38,44 @@ const io = new Server(server, {
 io.on("connection", async (socket) => {
     // console.log(socket.handshake, "socket");
     // console.log(socket.handshake.auth, "sockkkkk")
-    // socket.join(socket.handshake.auth._id);
+    socket.join(socket.handshake.auth._id);
+
     socket.emit("connection", "Hey you are connected");
+
+    socket.on("join-chat-room", ({ roomId }) => {
+        socket.join(roomId);
+    })
+
     socket.on("pvt-message", (msg) => {
         console.log(msg, "msg")
-        socket.join(msg.chatId);
-        io.to(msg.chatId).emit("pvt-message",
-            msg
-        )
+
+        const clientsInRoom = io.sockets.adapter.rooms.get(msg.chatId);
+        const isMultipleClients = clientsInRoom && clientsInRoom.size > 1;
+        console.log(clientsInRoom, "clinets");
+        if (isMultipleClients) {
+            io.to(msg.chatId).emit("pvt-message", msg);
+        }else{
+            socket.join(msg.toId);
+            io.to(msg.toId).emit("pvt-message", msg);
+        }
+
+
+        // // socket.join(msg.chatId);
+        // socket.join(msg.toId)
+
+        // console.log(clientsInRoom, "clinets")
+        // if (!isMultipleClients) {
+        //     io.to(msg.toId).emit("pvt-message", msg) 
+
+        //     // io.to(msg.chatId).emit("pvt-message", msg)
+        // } else {
+        //     io.to(msg.toId).emit("pvt-message", msg)
+        // }
+    })
+
+    socket.on("leave-room", ({ roomId }) => {
+        console.log("leaving", roomId)
+        socket.leave(roomId)
     })
 
     //disconnect event
